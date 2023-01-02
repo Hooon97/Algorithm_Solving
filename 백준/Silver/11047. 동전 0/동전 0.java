4d7fe901932f2b90754edc6e4fa10255279ced4d
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2023-01-02 15:16:03 +0900

    [Silver IV] Title: 동전 0, Time: 112 ms, Memory: 12868 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int sum = sc.nextInt();
-		int[] pennies = new int[N];
-		for(int i = 0; i<N; i++) pennies[i] = sc.nextInt();
-		int cnt = 0;
+		int N = sc.nextInt(); // 동전 종류의 개수
+		int K = sc.nextInt(); // 동전의 합
+		int[] coin = new int[N];
+		int count = 0;
+		for(int i = 0; i<N; i++)
+			coin[i] = sc.nextInt();
 		for(int i = N-1; i>=0; i--) {
-			if(pennies[i] > sum) continue;
-			else {
-				cnt += sum/pennies[i];
-				sum %= pennies[i];
+			int cur = coin[i];
+			if(cur <= K) {
+				count += K/cur;
+				K = K % cur;
 			}
 		}
-		System.out.print(cnt);
+		System.out.println(count);
 		sc.close();
 	}
 }
