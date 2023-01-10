8b47f90737e2405208e26bd74ba44aed05493b32
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2023-01-10 09:03:28 +0900

    [Silver I] Title: 경로 찾기, Time: 156 ms, Memory: 15112 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,34 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		int[][] map = new int[T][T];
-		
-		for(int i = 0; i<T; i++) {
-			for(int j = 0; j<T; j++) {
-				map[i][j] = sc.nextInt();
+	static int N;
+	static int[][] distance;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		distance = new int[N][N];
+		for(int i = 0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j<N; j++) {
+				int v = Integer.parseInt(st.nextToken());
+				distance[i][j] = v;
 			}
 		}
 		
-		
-		for(int i = 0; i<T; i++) { // 행
-			for(int j = 0; j<T; j++) { // 시작 노드
-				for(int p = 0; p<T; p++) { // 도착 노드
-					if(map[j][i] == 1 && map[i][p] == 1)
-						map[j][p] = 1;
+		//변형된 플로이드-워셜 알고리즘
+		//여기서 k는 중간에 거쳐가는 노드를 뜻함
+		for(int k = 0; k<N; k++) { // 모든 노드에 대하여
+			for(int i = 0; i<N; i++) { // 시작 노드
+				for(int j = 0; j<N; j++) { // 종료 노드
+					if(distance[i][k] == 1 && distance[k][j] == 1) distance[i][j] = 1;
 				}
 			}
 		}
 		
-		for(int i = 0; i<T; i++) {
-			for(int j = 0; j<T; j++) {
-				System.out.print(map[i][j] +" ");
+		for(int i = 0; i<N; i++) {
+			for(int j = 0; j<N; j++) {
+				sb.append(distance[i][j]+" ");
 			}
-			System.out.println();
+			sb.append("\n");
 		}
-		
-		sc.close();
+		System.out.print(sb.toString());
 	}
 }
