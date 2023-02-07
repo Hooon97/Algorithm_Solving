69ed99dcc44fd7739372413151ba1c78a545093d
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2023-02-07 21:26:53 +0900

    [Silver V] Title: 수들의 합 5, Time: 152 ms, Memory: 12868 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,34 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-/*
- * TAG: 투포인터
- * 앞에서부터 모든 경우의 수를 그리디로 탐색하고 싶을 때 사용하면 좋을듯..?
- */
-public class Main {
+public class Main{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int N = sc.nextInt();
+		
 		int st_idx = 1;
 		int ed_idx = 1;
 		int sum = 1;
-		int cnt = 1; // ed_idx가 N일 때를 미리 카운트
+		int ans = 1; // 자기 자신
+		//투포인터
 		while(ed_idx != N) {
 			if(sum == N) {
-				cnt++;
+				ans++;
 				ed_idx++;
 				sum += ed_idx;
 			}
-			else if(sum > N) {
+			else if(sum < N) {
+				//늘려야 하므로,
+				ed_idx++;
+				sum += ed_idx;
+			}
+			else {
+				//큰 경우는 줄여야 함
 				sum -= st_idx;
 				st_idx++;
 			}
-			else {
-				ed_idx++;
-				sum += ed_idx;
-			}
 		}
-		System.out.print(cnt);
-		sc.close();
+		System.out.print(ans);
+		
 	}
 }
