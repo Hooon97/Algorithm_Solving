383c14536ac3b28b6a0b251eb483dfda0034d212
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2023-02-07 21:56:17 +0900

    [Gold IV] Title: 좋다, Time: 124 ms, Memory: 12956 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,35 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] numb = new int[N];
-		for(int i = 0; i<N; i++) numb[i] = sc.nextInt();
-		Arrays.sort(numb);
+public class Main{
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.valueOf(br.readLine().trim());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] nums = new int[N];
+		for(int i = 0; i<N; i++) nums[i] = Integer.valueOf(st.nextToken());
+		Arrays.sort(nums);
 		
-		int cnt = 0;
-		for(int i = 0; i<N; i++) {
-			int find = numb[i]; // 검사할 숫자
-			int stIdx = 0;
-			int edIdx = N-1; 
-			
-			while(stIdx < edIdx) { // 투포인터 알고리즘
-				long sum = numb[stIdx] + numb[edIdx];
-				if(sum == find) { // 각 숫자가 find와 같은 수면 안됨
-					if(stIdx != i && edIdx != i) {
-						cnt++; // 다른 위치의 숫자일때만 가산
+		long ans = 0;
+		for(int k = 0; k<N; k++) {
+			int i = 0;
+			int j = N-1;
+			while(i<j) {
+				long cur = nums[i] + nums[j];
+				if(cur == nums[k]) {
+					//합이 같아도 자기 자신이면 안됨
+					if(i != k && j != k) {
+						ans++;
 						break;
 					}
-					else if(stIdx == i) stIdx++;
-					else if(edIdx == i) edIdx--;
+					else if(i == k) i++;
+					else if(j == k) j--;
 				}
-				else if(sum < find) stIdx++;
-				else edIdx--;
+				//더한 값이 더 작으면 크기를 키워야 함
+				else if(cur < nums[k]) {
+					i++;
+				}
+				//더한 값이 더 크면 크기를 작게해야 함
+				else j--;
 			}
 		}
-		System.out.print(cnt);
-		sc.close();
+		System.out.print(ans);
 	}
 }
