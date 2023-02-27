a1d5fd00ac9305b0f00c15fb684521f202cc36d9
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2023-02-27 17:53:48 +0900

    [Gold V] Title: 집합의 표현, Time: 504 ms, Memory: 55412 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,43 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main {
-	public static int[] parent;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+public class Main{
+	static int[] sets;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int N = sc.nextInt(); //최대 수의 크기
-		int M = sc.nextInt(); //연산의 개수
-		parent = new int[N+1];
-		for(int i = 0; i<N+1; i++) parent[i] = i;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.valueOf(st.nextToken());
+		int M = Integer.valueOf(st.nextToken());
+		sets = new int[N+1];
+		for(int i = 0; i<N+1; i++) sets[i] = i;
 		for(int i = 0; i<M; i++) {
-			int quest = sc.nextInt();
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			if(quest == 0) {
-				union(a,b);
-			}
-			else {
-				if(checkSame(a,b)) sb.append("YES\n");
-				else sb.append("NO\n");
+			st = new StringTokenizer(br.readLine());
+			int cal = Integer.valueOf(st.nextToken());
+			int num1 = Integer.valueOf(st.nextToken());
+			int num2 = Integer.valueOf(st.nextToken());
+			switch(cal) {
+			case 0 : Union(num1, num2);
+				break;
+			case 1 : 
+				if(Find(num1) == Find(num2))
+					sb.append("YES\n");
+				else
+					sb.append("NO\n");
+				break;
 			}
 		}
 		System.out.print(sb.toString());
 	}
-	static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-		if( a != b) {
-			parent[b]=a;
+	static void Union(int num1, int num2) {
+		num1 = Find(num1);
+		num2 = Find(num2);
+		if(num1 != num2) {
+			sets[num2] = num1;
 		}
 	}
-	static int find(int a) {
-		if(a == parent[a]) return a;
-		else return parent[a] = find(parent[a]);
-	}
-	static boolean checkSame(int a, int b) {
-		a = find(a);
-		b = find(b);
-		if(a==b) return true;
-		else return false;
+	static int Find(int num1) {
+		if(num1 == sets[num1]) return num1;
+		else return sets[num1] = Find(sets[num1]);
 	}
 }
