dbf9a22355e69300319f31ade3f5fdca81c285fb
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2024-08-01 23:26:45 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 37.56 ms, Memory: 98.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        for(int i = 0; i<n/2; i++)
+            answer += "수박";
+        answer += n%2 == 0 ? "" : "수";
+        
+        return answer;
+    }
+}
