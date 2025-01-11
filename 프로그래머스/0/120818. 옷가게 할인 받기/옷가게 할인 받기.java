49a89217fb10ee3604bdac89559f3b5b5c846b32
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2025-01-11 15:14:22 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int price) {
+        if(price < 100000) return price;
+        else if(price < 300000) return (int)(price * 0.95);
+        else if(price < 500000) return (int)(price * 0.9);
+        else return (int)(price * 0.8);
+    }
+}
