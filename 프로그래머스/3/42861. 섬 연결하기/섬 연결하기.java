e7949cf1304bd172f20c2f1a09b998011c805ee3
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2025-01-28 17:21:35 +0900

    [level 3] Title: 섬 연결하기, Time: 1.40 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,53 +1,55 @@
 import java.util.*;
 
 class Solution {
+    /*
+        Kruskal Algorithm
+    */
+    int[] parent;
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        List<Edge>[] graph = new ArrayList[n];
-        for(int i = 0; i<n; i++){
-            graph[i] = new ArrayList<>();
+        parent = new int[101];
+        for(int i = 0; i<101; i++) parent[i] = i;
+        PriorityQueue<Edge> pq = new PriorityQueue<>((o1,o2)->o1.w-o2.w);
+        
+        for(int i = 0; i<costs.length; i++){
+            int[] cost = costs[i];
+            Edge edge = new Edge(cost[0], cost[1], cost[2]);
+            pq.add(edge);
         }
-        
-        boolean[] visit = new boolean[n];
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        
-        for(int[] cost : costs){
-            int s = cost[0];
-            int e = cost[1];
-            int v = cost[2];
-            graph[s].add(new Edge(e,v));
-            graph[e].add(new Edge(s,v));
-        }
-        
-        pq.add(new Edge(0,0));
         
         while(!pq.isEmpty()){
-            Edge cur = pq.poll();
-            if(visit[cur.e]) continue;
-            
-            answer += cur.v;
-            visit[cur.e] = true;
-            for(Edge e : graph[cur.e]){
-                if(visit[e.e]) continue;
-                pq.add(e);
+            Edge edge = pq.poll();
+            if(find(edge.s) != find(edge.e)){
+                answer += edge.w;
+                union(edge.s, edge.e);
             }
-                
         }
-
+        
         return answer;
     }
     
-    class Edge implements Comparable<Edge>{
-        int e;
-        int v;
-        Edge(int e, int v){
-            this.e = e;
-            this.v = v;
-        }
+    public int find(int idx){
+        if(parent[idx] == idx) return idx;
         
-        @Override
-        public int compareTo(Edge e){
-            return this.v < e.v ? -1 : 1;
+        return parent[idx] = find(parent[idx]);
+    }
+    
+    public void union(int s, int e){
+        int a = find(s);
+        int b = find(e);
+        if(a != b)
+            parent[a] = b;
+    }
+    
+    class Edge{
+        int s;
+        int e;
+        int w;
+        
+        Edge(int s, int e, int w){
+            this.s = s;
+            this.e = e;
+            this.w = w;
         }
     }
 }
