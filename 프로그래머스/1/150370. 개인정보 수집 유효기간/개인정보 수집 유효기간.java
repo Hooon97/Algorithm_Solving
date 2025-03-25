5f32a1153861ae3ae145ac05217aa16326d98d3f
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2025-03-25 21:15:47 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 48.17 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,70 +1,45 @@
 import java.util.*;
+import java.text.SimpleDateFormat;
 
 class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) {
+    /*
+        1. today => date 기준으로 변환
+        2. terms => Map or Array에 저장 후 find
+        3. privacies를 돌며 최종 유효 기간 계산
+            => date로 변환 후 today와 비교.
+            => today보다 최종 유효 기간이 긴 경우 지나가고, 아니면 저장.
+    */
+    public int[] solution(String today, String[] terms, String[] privacies) throws Exception{
         int[] answer = {};
-        List<Integer> list = new LinkedList<>();
-        Map<String, Integer> term = getTermsMapper(terms);
-        int[] todayInt = convertDateToInt(today);
-        
-        for(int i = 0; i<privacies.length; i++){
-            String[] info = privacies[i].split(" ");
-            int[] curDate = convertDateToInt(info[0]);
-            calculateDate(curDate, term.get(info[1]));
-            if(deletable(todayInt, curDate)){
-                list.add(i+1);
-            }
+        Map<String, Integer> termMap = new HashMap<>();
+        for(String term : terms){
+            String[] termArr = term.split(" ");
+            termMap.put(termArr[0], Integer.valueOf(termArr[1]));
         }
         
-        answer = new int[list.size()];
-        for(int i = 0; i<list.size(); i++) answer[i] = list.get(i);
+        List<Integer> ansList = new ArrayList<>();
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+        Date toDate = sdf.parse(today);
+
+        Calendar cal = Calendar.getInstance();
+        int idx = 1;
+        for(String privacy : privacies){
+            String[] data = privacy.split(" ");
+            Date dataDate = sdf.parse(data[0]);
+            cal.setTime(dataDate);
+            cal.add(Calendar.MONTH, termMap.get(data[1]));
+            dataDate = cal.getTime();
+
+            if(!toDate.before(dataDate)){
+                ansList.add(idx);
+            }
+            idx++;
+        }
+        
+        answer = new int[ansList.size()];
+        for(int i = 0; i<answer.length; i++) answer[i] = ansList.get(i);
         
         return answer;
-    }
-    
-    public boolean deletable(int[] today, int[] data){
-        int curDate = today[0]*12*28 + today[1]*28 + today[2];
-        int dataDate = data[0]*12*28 + data[1]*28 + data[2];
-        
-        return curDate > dataDate;
-    }
-    
-    public void calculateDate(int[] date, int month){
-        date[1] += month;
-        date[2] -= 1;
-        
-        if(date[2] == 0){
-            date[1]--;
-            date[2] = 28;
-        }
-        
-        if(date[1] <= 0){
-            date[0]--;
-            date[1] = 12+date[1];
-        } else if(date[1] > 12){
-            date[0]++;
-            date[1] -= 12;
-        }
-        
-        return;
-    }
-    
-    public int[] convertDateToInt(String date){
-        String[] tmp = date.split("\\.");
-        int[] result = new int[3];
-        for(int i = 0; i<3; i++){
-            result[i] = Integer.valueOf(tmp[i]);
-        }
-        return result;
-    }
-    
-    public Map<String, Integer> getTermsMapper(String[] terms){
-        Map<String, Integer> map = new HashMap<>();
-        for(int i = 0; i<terms.length; i++){
-            String[] e = terms[i].split(" ");
-            map.put(e[0], Integer.valueOf(e[1]));
-        }
-        
-        return map;
     }
 }
